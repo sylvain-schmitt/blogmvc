68084ad5ac407324ec0b5e65c4ae50509a296c05
sylvain-schmitt <69637905+sylvain-schmitt@users.noreply.github.com>
2020-10-28 18:04:00 +0100

    Refactoring de la pagination
    Ajout d'une classe pour gérer la pagination

--- views/category/show.php
+++ views/category/show.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\URL;
 use App\Connection;
 use App\Model\Post;
 use App\Model\Category;
+use App\PaginatedQuery;
 
 $id = (int)$params['id'];
 $slug = $params['slug'];
@@ -30,30 +30,18 @@ if($category->getSlug() !== $slug) {
 
 $title = "Catégorie {$category->getName()}";
 
-/**
- * requète sur la table 'post' pour récupérer les articles de la catégorie concèrnée et les positionné 12 par page
- */
-$currentPage = URL::getPositiveInt('page', 1);
-$count = (int)$pdo
-    ->query('SELECT COUNT(category_id) FROM post_category WHERE category_id = ' . $category->getId())
-    ->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if ($currentPage > $pages) {
-  throw new Exception('Cette page n\'existe pas');
-}
-$offset = $perPage * ($currentPage - 1);
-$query = $pdo->query("
-SELECT p.* 
-FROM post p
-JOIN post_category pc ON pc.post_id = p.id
-WHERE pc.category_id = {$category->getId()}
-ORDER BY created_at DESC 
-LIMIT  $perPage OFFSET $offset
-");
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+//paramètre pour la pagination pour les articles par catégorie
+$paginatedQuery = new PaginatedQuery(
+  "SELECT p.* 
+      FROM post p
+      JOIN post_category pc ON pc.post_id = p.id
+      WHERE pc.category_id = {$category->getId()}
+      ORDER BY created_at DESC",
+    "SELECT COUNT(category_id) FROM post_category WHERE category_id =  {$category->getId()}",
+);
+/** @var Post[] */
+$posts = $paginatedQuery->getItems(Post::class);
 $link = $router->url('category', ['id' => $category->getId(), 'slug' => $category->getSlug()]);
-
 ?>
 
 <h1>Catégorie <?= e($title) ?></h1>
@@ -71,15 +59,7 @@ $link = $router->url('category', ['id' => $category->getId(), 'slug' => $categor
 
 <!--début pagination-->
 <div class="d-flex justify-content-between my-4">
-      <?php if($currentPage > 1): ?>
-        <?php
-        $l = $link;
-        if($currentPage > 2) $l = $link .  '?page=' . ($currentPage - 1);
-        ?>
-        <a href="<?= $l ?>"class ="btn btn-primary" > &laquo; Page précédente</a>
-      <?php endif ?>
-      <?php if($currentPage < $pages): ?>
-        <a href="<?= $link ?>?page=<?=$currentPage + 1 ?>"class ="btn btn-primary ml-auto" >  Page suivante &raquo;</a>
-      <?php endif ?>
+      <?= $paginatedQuery->previousLink($link) ?>
+      <?= $paginatedQuery->nextLink($link) ?>
 </div>
 <!--fin pagination-->
--- views/post/index.php
+++ views/post/index.php
@@ -3,6 +3,7 @@ use App\Helpers\Text;
 use App\URL;
 use App\Connection;
 use App\Model\Post;
+use App\PaginatedQuery;
 
 //variable pour geré le titre par page
 $title ='Mon Blog';
@@ -10,20 +11,14 @@ $title ='Mon Blog';
 //nouvelle instance de connection à la base de donnée
 $pdo = Connection::getPDO();
 
-
-/**
- * requète sur la table 'post' pour récupérer les articles et les positionné 12 par page
- */
-$currentPage = URL::getPositiveInt('page', 1);
-$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if ($currentPage > $pages) {
-  throw new Exception('Cette page n\'existe pas');
-}
-$offset = $perPage * ($currentPage - 1);
-$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT  $perPage OFFSET $offset");
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+//paramètre pour la pagination de tous les articles
+$paginatedQuery = new PaginatedQuery(
+    "SELECT * FROM post ORDER BY created_at DESC",
+    "SELECT COUNT(id) FROM post",
+);
+/** @var Post[] */
+$posts = $paginatedQuery->getItems(Post::class);
+$link = $router->url('home');
 ?>
 
 <h1>Bienvenue sur mon blog</h1>
@@ -41,15 +36,7 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 
 <!--début pagination-->
 <div class="d-flex justify-content-between my-4">
-      <?php if($currentPage > 1): ?>
-        <?php
-        $link = $router->url('home');
-        if($currentPage > 2) $link .= '?page=' . ($currentPage - 1);
-        ?>
-        <a href="<?= $link ?>"class ="btn btn-primary" > &laquo; Page précédente</a>
-      <?php endif ?>
-      <?php if($currentPage < $pages): ?>
-        <a href="<?= $router->url('home')?>?page=<?=$currentPage + 1 ?>"class ="btn btn-primary ml-auto" >  Page suivante &raquo;</a>
-      <?php endif ?>
+      <?= $paginatedQuery->previousLink($link);?>
+      <?= $paginatedQuery->nextLink($link);?>
 </div>
 <!--fin pagination-->
